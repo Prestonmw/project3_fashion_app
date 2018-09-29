0a17739de812421b4850e5f690e04b7e663e23ec
Deb Totten <deborahrtotten@gmail.com>
2018-09-29 11:58:56 -0400

    trying to get updated version

--- client/src/App.js
+++ client/src/App.js
@@ -13,50 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div className="container wrapper">
-      <div className="row">
-          <div className="panel panel-default">
-              <div className="panel-heading">
-                  <span className="traitHeader">Gender</span>
-              </div>
-              <div className="panel-body">
-                      <column className="col-md-12 text-center" id="genderColumn"></column>
-              </div>
-          </div>
-          <div className="panel panel-default">
-              <div className="panel-heading">
-                      <span className="traitHeader">Body Type</span>
-              </div>
-              <div className="panel-body">
-                      <column className="col-md-12 text-center" id="bodytypeColumn"></column>
-              </div>
-          </div>
-          <div className="panel panel-default">
-              <div className="panel-heading">
-                      <span className="ingredientHeader">Skin Tone</span>
-              </div>
-              <div className="panel-body">
-                      <column className="col-md-12 text-center" id="skintoneColumn"></column>
-              </div>
-          </div>
-          <div className="panel panel-default">
-              <div className="panel-heading">
-                      <span className="ingredientHeader">Height</span>
-              </div>
-              <div className="panel-body">
-                      <column className="col-md-12 text-center" id="heightColumn"></column>
-              </div>
-          </div>
-          <div className="panel panel-default">
-              <div className="panel-heading">
-                  <span className="ingredientHeader">Hair Color</span>
-              </div>
-              <div className="panel-body">
-                      <column className="col-md-12 text-center" id="haircolorColumn"></column>
-              </div>
-</div>
-</div>
-</div>
 </div>
     
     );

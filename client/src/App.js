dfae8508ae076d7be6c43cf737af73cb43d3ca91
Deb Totten <deborahrtotten@gmail.com>
2018-09-29 11:29:05 -0400

    learning react

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,59 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Bourgeousie</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        <div className="container wrapper">
+      <div className="row">
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                  <span className="traitHeader">Gender</span>
+              </div>
+              <div className="panel-body">
+                      <column className="col-md-12 text-center" id="genderColumn"></column>
+              </div>
+          </div>
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                      <span className="traitHeader">Body Type</span>
+              </div>
+              <div className="panel-body">
+                      <column className="col-md-12 text-center" id="bodytypeColumn"></column>
+              </div>
+          </div>
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                      <span className="ingredientHeader">Skin Tone</span>
+              </div>
+              <div className="panel-body">
+                      <column className="col-md-12 text-center" id="skintoneColumn"></column>
+              </div>
+          </div>
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                      <span className="ingredientHeader">Height</span>
+              </div>
+              <div className="panel-body">
+                      <column className="col-md-12 text-center" id="heightColumn"></column>
+              </div>
+          </div>
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                  <span className="ingredientHeader">Hair Color</span>
+              </div>
+              <div className="panel-body">
+                      <column className="col-md-12 text-center" id="haircolorColumn"></column>
+              </div>
+</div>
+</div>
+</div>
+</div>
+    
     );
   }
 }
+
+export default App;

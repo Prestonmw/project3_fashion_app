3fefa4dc697b0010f00ee4baa11d1670a690222c
Deb Totten <deborahrtotten@gmail.com>
2018-10-08 19:55:19 -0400

    trying to get latest

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./src/home/container";
 import Womans from "./src/womans/container";
 import Mens from "./src/mens/container";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
+      {/* <Nav /> */}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/womans" component={Womans} />
         <Route exact path="/mens" component={Mens} />
-        <Route component={NoMatch} />
+        {/* <Route component={NoMatch} /> */}
       </Switch>
     </div>
   </Router>

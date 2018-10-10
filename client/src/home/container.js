42a5b0a96f97650439b204a44e5351f9f4ba8bca
Nick Iantosca <niantosca805@gmail.com>
2018-10-10 18:44:28 -0400

    Men's page/needs style tweaking/added images folder under mens

--- client/src/home/container.js
+++ client/src/home/container.js
@@ -19,7 +19,7 @@ class Home extends Component {
           <div className="container">
             <div className="row">
               <div className="col-6">
-                <Example title="Men's" subtitle="World!" text="Hello again" buttonText="Click Me" image="https://cdn.shopify.com/s/files/1/0981/8178/files/editor-blue-oxford-freelancer-utility-shirt-left-hero_1024x1024.jpg?13256324731859498008" />
+                <Example title="Men's" subtitle="" text="Hello again" buttonText="Click Me" btnhref="/mens" image="https://cdn.shopify.com/s/files/1/0981/8178/files/editor-blue-oxford-freelancer-utility-shirt-left-hero_1024x1024.jpg?13256324731859498008" />
               </div>
               <div className="col-6">
                 <Example title="Women's" subtitle="World!" text="Goodbye again" buttonText="Don't Click Me" image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1041355480.jpg?crop=1.00xw:0.752xh;0,0.00240xh&resize=980:*" />

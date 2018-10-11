cc80dc5afac3458bcc6ab823dfa78dfc927698af
Deb Totten <deborahrtotten@gmail.com>
2018-10-10 20:28:40 -0400

    updating to pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
-import Home from "./src/home/container";
-import Womans from "./src/womans/container";
-import Mens from "./src/mens/container";
-// import NoMatch from "./pages/NoMatch";
-=======
 import Home from "./home/container";
 import Womans from "./womans/container";
 import Mens from "./mens/container";
->>>>>>> origin
 // import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
     <div>
-      {/* <Nav /> */}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/womans" component={Womans} />
         <Route exact path="/mens" component={Mens} />
-<<<<<<< HEAD
-        {/* <Route component={NoMatch} /> */}
-=======
->>>>>>> origin
       </Switch>
     </div>
   </Router>

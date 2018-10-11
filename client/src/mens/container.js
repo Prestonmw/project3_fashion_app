eae64e59d994cc7b038a5a46af2ca24c3da2f494
Deb Totten <deborahrtotten@gmail.com>
2018-10-11 18:51:29 -0400

    updating logo and woman's pics

--- client/src/mens/container.js
+++ client/src/mens/container.js
@@ -7,44 +7,45 @@ import Example from "../home/components/card/cardBtn";
 
 class mens extends Component {
     state = {
-       images: [
-           "./images/big_guy.jpg",
-           "./images/average1.jpg",
-           "./images/tall1.jpg"
-       ]
+        images: [
+            "./images/big_guy.jpg",
+            "./images/average1.jpg",
+            "./images/tall1.jpg",
+            "./images/Bouge_2.png"
+        ]
     };
 
-    
+
     render() {
         return (
-        <div className="container">
-            <div className="Home">
-                <div className="Home-header">
-              {/* <img src={logo} className="Home-logo" alt="logo" /> */}
-                     <h2>Bourgeois</h2>
-            </div>
-                <p className="Mens-intro">
-                     Welcome! Please choose your body-type below to get started!
+            <div className="container">
+                <div className="Home">
+                    <div className="Home-header">
+                        <img src={this.state.images[3]} />
+                    </div>
+                    <p className="Mens-intro">
+                        Welcome! Please choose your body-type below to get started!
                 </p>
-            </div>
-            <div className="row">
-                <div className="col-4">
-                    <Example title="Big Fellas" buttonText="That's Me!" btnhref="/mens" image={this.state.images[0]}  />
                 </div>
-                <div className="col-4">
-                    <Example title="Middle of the Road" buttonText="That's Me!" btnhref="/mens" image={this.state.images[1]} />
+                <div className="row">
+                    <div className="col-4">
+                        <Example title="Big Fellas" buttonText="That's Me!" btnhref="/mens" image={this.state.images[0]} />
+                    </div>
+                    <div className="col-4">
+                        <Example title="Middle of the Road" buttonText="That's Me!" btnhref="/mens" image={this.state.images[1]} />
+                    </div>
+                    <div className="col-4">
+                        <Example title="Skinny Non-fat Tall Hombre" buttonText="That's Me!" btnhref="/mens" image={this.state.images[2]} />
+                    </div>
                 </div>
-                <div className="col-4">
-                    <Example title="Skinny Non-fat Tall Hombre" buttonText="That's Me!" btnhref="/mens" image={this.state.images[2]} />
-                </div>
+
+
+
             </div>
 
-            
-            
-        </div>
 
-        
         )
-    
-    }};
+
+    }
+};
 export default mens;
--- client/src/womans/container.js
+++ client/src/womans/container.js
@@ -6,29 +6,35 @@ import Example from "../home/components/card/cardBtn";
 
 
 class womans extends Component {
-
+    state = {
+       images: [
+           "./images/pretty_plus.jpg",
+           "./images/medium_size.jpg",
+           "./images/petite.jpg",
+           "./images/Bouge_2.png"
+       ]
+    };
     
     render() {
         return (
         <div className="container">
             <div className="Home">
                 <div className="Home-header">
-              {/* <img src={logo} className="Home-logo" alt="logo" /> */}
-                     <h2>Bourgeousie</h2>
-            </div>
+                        <img src={this.state.images[3]} />
+                    </div>
                 <p className="Womans-intro">
                      Welcome! Please choose your body-type below to get started!
                 </p>
             </div>
             <div className="row">
                 <div className="col-4">
-                    <Example title="More to Adore" buttonText="That's Me!" btnhref="/womans" image="images/pretty_plus.jpg"/>
+                    <Example title="More to Adore" buttonText="That's Me!" btnhref="/womans" image={this.state.images[0]}/>
                 </div>
                 <div className="col-4">
-                    <Example title="Midling Sort" buttonText="That's Me!" btnhref="/womans" image="images/medium_size.jpg"/>
+                    <Example title="Midling Sort" buttonText="That's Me!" btnhref="/womans" image={this.state.images[1]}/>
                 </div>
                 <div className="col-4">
-                    <Example title="Pretty and Petite" buttonText="That's Me!" btnhref="/womans" image="images/petite.jpg"/>
+                    <Example title="Pretty and Petite" buttonText="That's Me!" btnhref="/womans" image={this.state.images[2]}/>
                 </div>
             </div>
 

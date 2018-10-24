de63744700b1e06f6875aea1a3f6cc427c6a1ce0
Nick Iantosca <niantosca805@gmail.com>
2018-10-23 20:31:38 -0400

    Pushing to pull

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./home/container";
 import Womans from "./womans/container";
 import Mens from "./mens/container";
+<<<<<<< HEAD
 import Bigmen from "./mens/bigman";
+=======
+import StylePage from "./common/style-page";
+
+
+>>>>>>> 6100ed2f638f08ebe8b203d66cada7a6fb11d9f1
 // import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
     <div>
-      {/* <Nav /> */}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/womans" component={Womans} />
         <Route exact path="/mens" component={Mens} />
+<<<<<<< HEAD
         <Route exact path="/bigmen" component={Bigmen} />
+=======
+        <Route exact path="/mensbig" render={(props) => <StylePage { ...props } category="mensbig"/>}  />
+        <Route exact path="/mensmedium" render={(props) => <StylePage { ...props } category="mensmedium"/>}  />
+        <Route exact path="/menssmall" render={(props) => <StylePage { ...props } category="menssmall"/>}  />
+        <Route exact path="/womansbig" render={(props) => <StylePage { ...props } category="womansbig"/>}  />
+        <Route exact path="/womansmedium" render={(props) => <StylePage { ...props } category="womansmedium"/>}  />
+        <Route exact path="/womanssmall" render={(props) => <StylePage { ...props } category="womanssmall"/>}  />
+>>>>>>> 6100ed2f638f08ebe8b203d66cada7a6fb11d9f1
       </Switch>
     </div>
   </Router>
--- client/src/home/container.js
+++ client/src/home/container.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "../styles/App.css";
-import Example from "./components/card/cardBtn";
+import StyleCard from "../common/components/card/style-card";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "../navbar/navbar";
 
 class Home extends Component {
   state = {
     images: [
         "./images/man_basic1.jpg",
-        "./images/woman_basic.jpg"
+        "./images/woman_basic.jpg",
+        "./images/Bouge_2.png"
     ]
  };
     render() {
       return (
+<<<<<<< HEAD
       <div className="container">
         <div className="Home">
             <div className="Home-header">
@@ -22,16 +25,37 @@ class Home extends Component {
               <p className="Home-intro">
                 Make a Selection Below!
               </p>
+=======
+        <div className="container">
+        <Navbar/>
+        <div className="Home">
+          <div className="Home-header">
+           <img src={this.state.images[2]}/>
+          </div>
+          <div className="row">
+          <div className="col-12">
+          <p className="Home-intro">
+          Welcome! Do you aspire to improve your look and further your lot in life? Click the picture that most resembles you to learn how!
+          </p>
+          </div>
+          </div>
+          
+          <div className="container">
+>>>>>>> 6100ed2f638f08ebe8b203d66cada7a6fb11d9f1
             <div className="row">
               <div className="col-6">
-                <Example title="Men's" subtitle="" text="" buttonText="Click Me" btnhref="/mens" image={this.state.images[0]} />
+                <StyleCard title="Men's" subtitle="" text="" buttonText="Click Me" btnhref="/mens" image={this.state.images[0]} />
               </div>
               <div className="col-6">
-                <Example title="Women's" subtitle="" text="" buttonText="Click Me" btnhref="/womans" image={this.state.images[1]} />
+                <StyleCard title="Women's" subtitle="" text="" buttonText="Click Me" btnhref="/womans" image={this.state.images[1]} />
               </div>
             </div>
         </div>
+<<<<<<< HEAD
       </div>
+=======
+        </div>
+>>>>>>> 6100ed2f638f08ebe8b203d66cada7a6fb11d9f1
       );
     }
   }
